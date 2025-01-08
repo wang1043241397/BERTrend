a53b8df4e0cea66f346bff3a3caae54110320587
Guillaume Grosjean <guillaume.grosjean@rte-france.com>
2025-01-08 16:10:10 +0100

    Cache loaded data when changing page

--- bertrend/demos/demos_utils/data_loading_component.py
+++ bertrend/demos/demos_utils/data_loading_component.py
@@ -133,9 +133,11 @@ def display_data_loading_component():
             on_change=save_widget_state,
         )
 
-    if not SessionStateManager.get(
-        "uploaded_files", []
-    ) and not SessionStateManager.get("selected_files"):
+    if (
+        not SessionStateManager.get("uploaded_files", [])
+        and not SessionStateManager.get("selected_files")
+        and "initial_df" not in st.session_state
+    ):
         st.warning(NO_DATASET_WARNING, icon=WARNING_ICON)
         st.stop()
 
@@ -150,115 +152,117 @@ def display_data_loading_component():
         dfs = _load_files(
             SessionStateManager.get("selected_files"),
         )
-    # Check if DataFrames have been found
-    if not dfs:
-        st.warning(
-            NO_DATA_AFTER_PREPROCESSING_MESSAGE,
-            icon=WARNING_ICON,
-        )
-    else:
+
+    # If DataFrames found, concatenate them
+    if dfs:
         # Concatenate DataFrames
         df = pd.concat(dfs, ignore_index=True)
 
         # Save state of initial DF (before split and data selection)
         st.session_state["initial_df"] = df.copy()
-
-        # Show raw data info
-        st.write(
-            f"Number of documents in raw data: **{len(st.session_state['initial_df'])}**"
+    # If dfs is None and there is no 'initial_df' in session_state, show warining
+    elif "initial_df" not in st.session_state:
+        st.warning(
+            NO_DATA_AFTER_PREPROCESSING_MESSAGE,
+            icon=WARNING_ICON,
         )
+        st.stop()
+    # If dfs is None but there is initial_df, set it
+    else:
+        df = st.session_state["initial_df"]
 
-        # Data filtering section
-        st.header("Data filtering")
+    # Show raw data info
+    st.write(
+        f"Number of documents in raw data: **{len(st.session_state['initial_df'])}**"
+    )
 
-        # Display number input and checkbox for preprocessing options
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            register_widget("min_chars")
-            st.number_input(
-                "Minimum Characters",
-                value=MIN_CHARS_DEFAULT,
-                min_value=0,
-                max_value=1000,
-                key="min_chars",
-                on_change=save_widget_state,
-                help="Minimum number of characters each document must contain.",
-            )
-        with col2:
-            register_widget("sample_size")
-            sample_size = st.number_input(
-                "Sample ratio",
-                value=SAMPLE_SIZE_DEFAULT,
-                min_value=0.0,
-                max_value=1.0,
-                key="sample_size",
-                on_change=save_widget_state,
-                help="Fraction of raw data to use for computing topics. Randomly samples documents from raw data.",
-            )
-        with col3:
-            register_widget("split_by_paragraph")
-            SessionStateManager.get_or_set("split_by_paragraph", "no")
-            st.segmented_control(
-                "Split text by paragraphs",
-                key="split_by_paragraph",
-                options=["no", "yes", "enhanced"],
-                selection_mode="single",
-                help="'No split': No splitting on the documents ; 'Split by paragraphs': Split documents into paragraphs ; "
-                "'Enhanced split': uses a more advanced but slower method for splitting that considers the embedding "
-                "model's maximum input length.",
-                on_change=save_widget_state,
-            )
+    # Data filtering section
+    st.header("Data filtering")
 
-        df = split_data(
-            df,
-            SessionStateManager.get("min_chars"),
-            SessionStateManager.get("split_by_paragraph"),
-            embedding_model_name=SessionStateManager.get("embedding_model_name"),
+    # Display number input and checkbox for preprocessing options
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        register_widget("min_chars")
+        st.number_input(
+            "Minimum Characters",
+            value=MIN_CHARS_DEFAULT,
+            min_value=0,
+            max_value=1000,
+            key="min_chars",
+            on_change=save_widget_state,
+            help="Minimum number of characters each document must contain.",
         )
-
-        # Deduplicate using all columns
-        df = df.drop_duplicates()
-
-        # Save state of split dataframe (before time-based filtering)
-        st.session_state["split_df"] = df.copy()
-
-        # Select timeframe
-        min_date, max_date = st.session_state["initial_df"][
-            TIMESTAMP_COLUMN
-        ].dt.date.agg(["min", "max"])
-        register_widget("timeframe_slider")
-        start_date, end_date = st.slider(
-            "Select Timeframe",
-            min_value=min_date,
-            max_value=max_date,
-            value=(min_date, max_date),
-            key="timeframe_slider",
+    with col2:
+        register_widget("sample_size")
+        sample_size = st.number_input(
+            "Sample ratio",
+            value=SAMPLE_SIZE_DEFAULT,
+            min_value=0.0,
+            max_value=1.0,
+            key="sample_size",
+            on_change=save_widget_state,
+            help="Fraction of raw data to use for computing topics. Randomly samples documents from raw data.",
+        )
+    with col3:
+        register_widget("split_by_paragraph")
+        SessionStateManager.get_or_set("split_by_paragraph", "no")
+        st.segmented_control(
+            "Split text by paragraphs",
+            key="split_by_paragraph",
+            options=["no", "yes", "enhanced"],
+            selection_mode="single",
+            help="'No split': No splitting on the documents ; 'Split by paragraphs': Split documents into paragraphs ; "
+            "'Enhanced split': uses a more advanced but slower method for splitting that considers the embedding "
+            "model's maximum input length.",
             on_change=save_widget_state,
         )
 
-        # Filter and sample the DataFrame
-        df_filtered = df[
-            (df[TIMESTAMP_COLUMN].dt.date >= start_date)
-            & (df[TIMESTAMP_COLUMN].dt.date <= end_date)
-        ]
-        df_filtered = df_filtered.sort_values(by=TIMESTAMP_COLUMN).reset_index(
-            drop=True
-        )
+    df = split_data(
+        df,
+        SessionStateManager.get("min_chars"),
+        SessionStateManager.get("split_by_paragraph"),
+        embedding_model_name=SessionStateManager.get("embedding_model_name"),
+    )
 
-        if sample_size < 1:
-            df_filtered = df_filtered.sample(frac=sample_size, random_state=42)
+    # Deduplicate using all columns
+    df = df.drop_duplicates()
 
-        df_filtered = df_filtered.sort_values(by=TIMESTAMP_COLUMN).reset_index(
-            drop=True
-        )
+    # Save state of split dataframe (before time-based filtering)
+    st.session_state["split_df"] = df.copy()
 
-        SessionStateManager.set("time_filtered_df", df_filtered)
-        st.write(
-            f"Number of documents in filtered data: **{len(SessionStateManager.get_dataframe('time_filtered_df'))}**"
-        )
-        st.dataframe(
-            SessionStateManager.get_dataframe("time_filtered_df")[
-                [TEXT_COLUMN, "timestamp"]
-            ],
-            use_container_width=True,
-        )
+    # Select timeframe
+    min_date, max_date = st.session_state["initial_df"][TIMESTAMP_COLUMN].dt.date.agg(
+        ["min", "max"]
+    )
+    register_widget("timeframe_slider")
+    start_date, end_date = st.slider(
+        "Select Timeframe",
+        min_value=min_date,
+        max_value=max_date,
+        value=(min_date, max_date),
+        key="timeframe_slider",
+        on_change=save_widget_state,
+    )
+
+    # Filter and sample the DataFrame
+    df_filtered = df[
+        (df[TIMESTAMP_COLUMN].dt.date >= start_date)
+        & (df[TIMESTAMP_COLUMN].dt.date <= end_date)
+    ]
+    df_filtered = df_filtered.sort_values(by=TIMESTAMP_COLUMN).reset_index(drop=True)
+
+    if sample_size < 1:
+        df_filtered = df_filtered.sample(frac=sample_size, random_state=42)
+
+    df_filtered = df_filtered.sort_values(by=TIMESTAMP_COLUMN).reset_index(drop=True)
+
+    SessionStateManager.set("time_filtered_df", df_filtered)
+    st.write(
+        f"Number of documents in filtered data: **{len(SessionStateManager.get_dataframe('time_filtered_df'))}**"
+    )
+    st.dataframe(
+        SessionStateManager.get_dataframe("time_filtered_df")[
+            [TEXT_COLUMN, "timestamp"]
+        ],
+        use_container_width=True,
+    )
